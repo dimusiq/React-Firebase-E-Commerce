1f1ac0fe079c1a9efa0e33b16e5d43340c7f6fb3
dimusiq <dumojke@gmail.com>
2022-03-06 22:27:23 +0500

    added-custom-button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component.jsx';
 import ShopPage from '../src/pages/shop/shop.component.jsx'
 import Header from './components/header/header.component';
-
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -15,7 +15,8 @@ function App() {
     <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/shop/hats' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+
+const SignInAndSignUp = () => (
+    <div className='sign'>
+        <SignIn />
+    </div>
+)
+
+
+export default SignInAndSignUp;
