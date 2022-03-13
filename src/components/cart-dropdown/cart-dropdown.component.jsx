26a32d5ef5e8ea61f161b2f036cb0095636b9d13
dimusiq <dumojke@gmail.com>
2022-03-13 18:06:06 +0500

    added addItem and removeItem to checkout-item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({ cartItems, history }) => (
+const CartDropDown = ({ cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
         {   
@@ -20,7 +21,12 @@ const CartDropDown = ({ cartItems, history }) => (
             <span className='empty-message'></span>
             }
             </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+            }}>
+            GO TO CHECKOUT
+            </CustomButton>
     </div>
 )
 
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,10 +4,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     if (existingCartItem) {
         return cartItems.map(cartItem => 
             cartItem.id === cartItemToAdd.id
-                ?{ ...cartItem, quantity: cartItem.quantity + 1}
+                ? { ...cartItem, quantity: cartItem.quantity + 1}
                 : cartItem
             )
     }
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1}]
 };
+
+export const removeItemFromCart = ( cartItems, cartItemsToRemove ) => {
+    const existingCartItem = cartItems.filter(
+        cartItem => cartItem.id === cartItemsToRemove.id
+    )
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemsToRemove.id)
+    }
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemsToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1}
+        :
+        cartItem
+    )
+}
