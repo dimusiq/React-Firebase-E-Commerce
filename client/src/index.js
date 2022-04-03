2f6a5ecadfab071467528a140a012fd9f3caaa7a
dimusiq <dumojke@gmail.com>
2022-04-04 00:06:05 +0500

    addin PWA support

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -24,10 +26,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
